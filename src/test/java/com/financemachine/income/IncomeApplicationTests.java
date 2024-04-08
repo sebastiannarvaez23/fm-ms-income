8102920246117ff3669608a6c249e519b517e7db
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2024-04-07 20:23:31 -0500

    Archivos iniciales del proyecto

---
+++ src/test/java/com/financemachine/income/IncomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financemachine.income;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
